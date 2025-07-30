741ece0eb292f81c68f6e6ed20e8ae57b88e39da
Ramin <rmnznd@gmail.com>
2025-07-30 07:35:34 -0700

    Initial Commit

---
+++ Word-Scramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Word-Scramble
+//
+//  Created by Ramin Zand on 7/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Word-Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word-Scramble
+//
+//  Created by Ramin Zand on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
